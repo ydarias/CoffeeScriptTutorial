f298a78a175302ff2b355ab8dee78957867debff
Yeray Darias <ydarias@gmail.com>
2012-07-01 11:20:13 +0100

    Creada la pantalla de configuración

--- client/modules/configuration.js
+++ client/modules/configuration.js
@@ -1,9 +1,20 @@
 Jaml.register('configuration', function () {
     div({cls:'hero-unit'},
-        h1('CoffeeScriptTutorial'),
-        p('Esta aplicación está pensada como un pequeño tutorial para aprender CoffeeScript, ' +
-            'a continuación se presentarán una serie de ejercicios que se han de resolver ' +
-            'utilizando dicho lenguaje ... pero antes de empezar asegurate que tienes correctamente ' +
-            'configurado el servidor de pruebas que deseas utilizar.')
+        h1('Configuración'),
+        p('Rellena los datos solicitados en esta página para poder hacer uso de la ' +
+            'aplicación')
+    ),
+
+    form({cls: 'well'},
+        label('Nombre (indica un nombre para guardar la sesión en el servidor)'),
+        input({id: 'name'}),
+        label('Dirección del servidor (ej: http://192.168.1.2:8080)'),
+        input({id: 'server'}),
+        p(
+            a({
+                cls: 'btn',
+                href:'javascript:saveConfiguration()'
+            }, 'Guardar')
+        )
     )
 });
